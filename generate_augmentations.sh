d9b51fe40bf19f06f46d92c45ded5f82dc68301d
Farhan Samir <fsamir8@cdr544.int.cedar.computecanada.ca>
2022-10-06 15:22:57 -0700

    Add script for generating augmentations for low-data condition of SIGMORPHON2018

---
+++ generate_augmentations.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# NOTE: this works!! just need to iterate over every language.
+DATA_PATH="/home/fsamir8/projects/rrg-msilfver/fsamir8/conll2018/task1/all/"
+# iterate through all files
+    # then, if it's a low-train, use it.
+
+for fname_full in "${DATA_PATH}"/*
+do
+    fname=$(basename "$fname_full")
+    if [[ $fname == *"train-low" ]]; then
+        python augment.py $DATA_PATH "$fname" --examples 10000
+    fi
+done
