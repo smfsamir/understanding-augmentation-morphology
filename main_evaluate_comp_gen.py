a30c90490a0f56691825ef5f07db86f3c1389c38
smfsamir <f.samir@mail.utoronto.ca>
2023-01-19 14:50:52 -0800

    Fix paths for comp gen evaluation

--- main_evaluate_comp_gen.py
+++ main_evaluate_comp_gen.py
@@ -17,10 +17,8 @@ def write_cg_test_frame(src_path, tgt_path, cg_test_frame: pd.DataFrame):
     with open(tgt_path, 'w') as cg_frame_f:
         cg_test_frame[['tgt']].apply(lambda row: cg_frame_f.write(f"{tokenize_row_tgt(row)}\n"), axis=1)
 
-def evaluate_on_cg_test_set(language):
-    # NOTE: i hope there's no characters in this expanded test set that are not in the vocab...
-    # TODO: write result into the same folder.
-    prefix = f"{SCRATCH_PATH}/{language}"
+def evaluate_on_cg_test_set(language, seed):
+    prefix = f"{SCRATCH_PATH}/{language}_seed={seed}"
     for dirname in tqdm.tqdm(os.listdir(prefix)):
         model_name = f"{prefix}/{dirname}/{language}_{dirname}_model_checkpoints/checkpoint_best.pt"
         model_preproc_dir = f"{prefix}/{dirname}/{language}_fairseq_bin" 
@@ -36,32 +34,29 @@ def evaluate_on_cg_test_set(language):
                 if 'model_checkpoints' in possible_model_dir: 
                     model_name = f"{prefix}/{dirname}/{possible_model_dir}/checkpoint_best.pt"
                     break
-        if 'train_medium=True' in dirname:
-            stdin = f"{SCRATCH_PATH}/{language}/cg_test_frame_medium_excluded_2.txt"
-            stdout = f"{SCRATCH_PATH}/{language}/{dirname}/cg_medium_excluded_2.txt"
-        elif 'train_medium=False' in dirname:
-            stdin = f"{SCRATCH_PATH}/{language}/cg_test_frame_low_excluded_2.txt"
-            stdout = f"{SCRATCH_PATH}/{language}/{dirname}/cg_low_excluded_2.txt"
+        stdin = f"{SCRATCH_PATH}/{language}_seed={seed}/cg_test_frame_low_excluded_2.txt"
+        stdout = f"{SCRATCH_PATH}/{language}_seed={seed}/{dirname}/cg_low_excluded_2.txt"
         
         if os.path.exists(stdout):
             print(f"Skipping because it appears that {stdout} has already been generated")
             continue
 
         stdout = open(stdout, 'w')
-        # subprocess.run(["fairseq-interactive", "--path", model_name, model_preproc_dir, 
-        #                 "--source-lang", "src", "--target-lang", "tgt", # probably for how to encode and decode...
-        #                 "--tokenizer", "space", "--buffer-size", "100"],
-        #                 stdin=stdin, stdout=stdout, check=True) 
+        stdin = open(stdin, 'w')
+        subprocess.run(["fairseq-interactive", "--path", model_name, model_preproc_dir, 
+                        "--source-lang", "src", "--target-lang", "tgt", # probably for how to encode and decode...
+                        "--tokenizer", "space", "--buffer-size", "100"],
+                        stdin=stdin, stdout=stdout, check=True) 
 
         #the required batch size multiple assumes that the number of sentences 
         # to be generated is a multiple of 4
-        subprocess.run(["fairseq-generate", model_preproc_dir, "--path", model_name, 
-                        "--batch-size", "128", "--beam", "5", "--required-batch-size-multiple", "4"], 
-                        stdout=stdout, check=True)
+        # subprocess.run(["fairseq-generate", model_preproc_dir, "--path", model_name, 
+        #                 "--batch-size", "128", "--beam", "5", "--required-batch-size-multiple", "4"], 
+        #                 stdout=stdout, check=True)
         print(f"Completed {dirname}")
 
 def write_test_frames(seed):
-    for language_quantity in tqdm.tqdm(product(LANGUAGES, [ 'medium'])): 
+    for language_quantity in tqdm.tqdm(product(LANGUAGES, ['medium'])):
         language = language_quantity[0]
         quantity = language_quantity[1]
         test_frame = construct_cg_test_set(language, quantity)
@@ -72,7 +67,7 @@ def main(args):
     seed = args.rand_seed
     write_test_frames(seed)
     for language in LANGUAGES:
-        evaluate_on_cg_test_set(language)
+        evaluate_on_cg_test_set(language, seed)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("rand_seed", type=int)
