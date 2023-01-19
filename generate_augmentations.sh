6ca8ac7cf2a1ed566299b589413d76f7284dbe9a
Farhan Samir <f.samir@mail.utoronto.ca>
2023-01-19 14:31:52 -0800

    Add random seed to pipelines

--- generate_augmentations.sh
+++ generate_augmentations.sh
@@ -4,6 +4,9 @@
 DATA_PATH="/home/fsamir8/projects/rrg-msilfver/fsamir8/conll2018/task1/all/"
 # iterate through all files
     # then, if it's a low-train, use it.
+    # TODO: just do it for the target languages
+    # TODO: then, do it for 
+LANGUAGES=("bengali" "turkish" "finnish" "georgian" "arabic" "navajo" "spanish")
 
 for fname_full in "${DATA_PATH}"/*
 do
--- main_transformer.py
+++ main_transformer.py
@@ -9,7 +9,6 @@ from collections import defaultdict
 import wandb
 
 from packages.utils.constants import SIGM_DATA_PATH, SCRATCH_PATH, FAIRSEQ_SCRIPTS_PATH, INITIAL_MODEL_PARAMS
-from packages.augmentation.initial_augmentations import add_copies
 from packages.augmentation.select_highest_loss import HighLossSampler
 from packages.augmentation.subset_selecter_strategy import get_subset_selecter
 from packages.fairseq_utils.dataloading_utils import get_initial_generation_frame, get_augmentation_example_lengths
@@ -35,8 +34,6 @@ def prep_preproc_fairseq_data_initial(language: str, augmentation_type: str, **k
 
     # TODO: would add the copies here.
     train_frame, validation_frame, test_frame = load_gold_train_validation_test(language, train_medium)
-    if kwargs['add_copies']:
-        train_frame = add_copies(train_frame)
 
     test_frame = get_initial_generation_frame(language)
 
@@ -52,7 +49,7 @@ def run_fairseq_binarizer(language, augmentation_type, **kwargs):
 def train_model(language, augmentation_type, **kwargs):
     model_augment_path = get_model_augment_path(language, augmentation_type, **kwargs)
     algorithm_type = model_augment_path.split('/')[-1]
-    result = subprocess.run([f"{FAIRSEQ_SCRIPTS_PATH}/train_model.sh", model_augment_path, language, algorithm_type])
+    result = subprocess.run([f"{FAIRSEQ_SCRIPTS_PATH}/train_model.sh", model_augment_path, language, algorithm_type, str(kwargs['rand_seed'])])
     print(f"Obtained {result} result")
 
 def generate(language, augmentation_type, **kwargs):
@@ -60,11 +57,6 @@ def generate(language, augmentation_type, **kwargs):
     algorithm_type = model_augment_path.split('/')[-1]
     result = subprocess.run([f"{FAIRSEQ_SCRIPTS_PATH}/generate.sh", model_augment_path, language, algorithm_type])
     print(f"Obtained {result} result")
-
-def rename_initial_embeddings(language, **kwargs):
-    model_augment_path = get_model_augment_path(language, 'initial', **kwargs)
-    result = subprocess.run([f"{FAIRSEQ_SCRIPTS_PATH}/mv_initial_embeddings.sh", model_augment_path, language])
-    print(f"Obtained {result} result for renaming initial embeddings")
 
 # TODO: delete this later; use from the util_functions module.
 def get_number_test_examples(language, **kwargs):
@@ -130,38 +122,7 @@ def extract_log_likelihoods(language, num_test_examples, **kwargs ):
             predictions_f.readline() # skip per token line
     with open(f"{initial_model_path}/{language}_log_likelihoods.pickle", "wb") as ll_handle: 
         pickle.dump(avg_log_likelihoods, ll_handle, protocol=pickle.HIGHEST_PROTOCOL)
-
     assert len(avg_log_likelihoods) == 10000, f"There were {len(avg_log_likelihoods)} log likelihoods collected."
-
-def probe_initial_representations(language, number_test_examples):
-    path = f"{SCRATCH_PATH}/{language}/initial"
-
-    token_id_to_embeds = defaultdict(list)
-    with open(f"{path}/{language}_embeddings.pickle", "rb") as embeddings_pkl, \
-         open(f"{path}/{language}_src_tokens.pickle", "rb") as token_ids_pkl, \
-         open(f"{path}/{language}_ids.pickle", "rb") as item_ids_pkl, \
-         open(f"{path}/{language}_src_dict.pickle", "rb") as src_dict_pkl:
-        embeddings_dict = pickle.load(embeddings_pkl)
-        item_id_dict = pickle.load(item_ids_pkl)
-        token_ids_dict = pickle.load(token_ids_pkl)
-        src_dict = pickle.load(src_dict_pkl)
-
-        processed_items = set([])
-        for seq_len in embeddings_dict.keys():
-            embeds = np.array(embeddings_dict[seq_len])
-            token_ids = np.array(token_ids_dict[seq_len])
-            item_ids = np.array(item_id_dict[seq_len])
-            for i in range(token_ids.shape[0]): # iterating over number of items
-                if item_ids[i] < number_test_examples:
-                    processed_items.add(item_ids[i])
-                    for j in range(token_ids.shape[1]): # iterating over sequence length
-                        token_id_to_embeds[token_ids[i, j]].append(embeds[j, i])
-        # TODO concatenate all of the embeddings in each token.
-        assert 0 in processed_items
-        # for k, v in token_id_to_embeds:
-        #     token_id_to_embeds[k] = np.vstack(v).to
-    with open(f"{path}/{language}_token_id_to_embeds.pickle", "wb") as handle:
-        pickle.dump(token_id_to_embeds, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 def get_initial_model_path(language, **kwargs):
     init_kwargs = {k: kwargs[k] for k in INITIAL_MODEL_PARAMS}
@@ -182,10 +143,10 @@ def prep_preproc_fairseq_data_augment(language, augmentation_type, **kwargs):
     _write_split(language, augmentation_type, validation_frame, "valid", **kwargs)
     _write_split(language, augmentation_type, test_frame, "test", **kwargs)
 
-def run_initial_pipeline(language: str, num_aug: int, train_medium: bool, use_copies: bool):
+def run_initial_pipeline(language: str, train_medium: bool, rand_seed: int):
     hparam_comb = {
-        "train_medium": train_medium,
-        "add_copies": use_copies 
+        "train_medium": train_medium, 
+        "rand_seed": rand_seed
     }
     prep_preproc_fairseq_data_initial(language, 'initial', **hparam_comb)
     run_fairseq_binarizer(language, 'initial', **hparam_comb)
@@ -194,20 +155,26 @@ def run_initial_pipeline(language: str, num_aug: int, train_medium: bool, use_co
     report_accuracy(language, 'initial', get_number_test_examples(language), **hparam_comb)
     extract_log_likelihoods(language, get_number_test_examples(language), **hparam_comb)
 
-def run_random_sampling_pipeline(language, num_aug, train_medium): 
-    prep_preproc_fairseq_data_augment(language, 'random', num_aug=num_aug, train_medium=train_medium)
-    run_fairseq_binarizer(language, 'random', num_aug=num_aug, train_medium=train_medium)
-    train_model(language, 'random', num_aug=num_aug, train_medium=train_medium)
-    generate(language, 'random', num_aug=num_aug, train_medium=train_medium)
-    report_accuracy(language, 'random', get_number_test_examples(language), num_aug=num_aug, train_medium=train_medium)
-
-def run_uncertainty_sampling_pipeline(language, num_aug, r=1, use_softmax_normalizer=False, use_high_loss=False, train_medium=False): 
+def run_random_sampling_pipeline(language, num_aug, train_medium, rand_seed): 
     hparam_comb = {
         "num_aug": num_aug,
-        "use_softmax_normalizer": use_softmax_normalizer,
-        "r": r,
+        "train_medium": train_medium, 
+        "rand_seed": rand_seed
+    }
+    prep_preproc_fairseq_data_augment(language, 'random', **hparam_comb)
+    run_fairseq_binarizer(language, 'random', **hparam_comb)
+    train_model(language, 'random', **hparam_comb)
+    generate(language, 'random', **hparam_comb)
+    report_accuracy(language, 'random', get_number_test_examples(language), **hparam_comb)
+
+def run_uncertainty_sampling_pipeline(language, num_aug, \
+                                      use_high_loss, train_medium, \
+                                      rand_seed): 
+    hparam_comb = {
+        "num_aug": num_aug,
         "use_high_loss": use_high_loss,
-        "train_medium": train_medium
+        "train_medium": train_medium,
+        "rand_seed": rand_seed
     }
     prep_preproc_fairseq_data_augment(language, 'uncertainty_sample', **hparam_comb)
     run_fairseq_binarizer(language, 'uncertainty_sample', **hparam_comb)
@@ -215,11 +182,13 @@ def run_uncertainty_sampling_pipeline(language, num_aug, r=1, use_softmax_normal
     generate(language, 'uncertainty_sample', **hparam_comb)
     report_accuracy(language, 'uncertainty_sample', get_number_test_examples(language), **hparam_comb)
 
-def run_uat_pipeline(language: str, num_aug: int, train_medium: bool, use_empirical: bool, use_loss: bool):
+def run_uat_pipeline(language: str, num_aug: int, train_medium: bool, \
+                     use_empirical: bool, use_loss: bool, rand_seed: int):
     hparam_comb = {
         "num_aug": num_aug,
         "use_empirical": use_empirical,
         "train_medium": train_medium,
+        "rand_seed": rand_seed,
         "use_loss": use_loss
     }
     algorithm = 'uat'
@@ -243,26 +212,26 @@ def main(args):
         generate(args.language, args.augmentation_type)
     elif args.report_accuracy:
         report_accuracy(args.language, args.augmentation_type, get_number_test_examples(args.language))
-    elif args.probe_initial_representations:
-        probe_initial_representations(args.language, get_number_test_examples(args.language))
     elif args.extract_log_likelihoods:
         extract_log_likelihoods(args.language, get_number_test_examples(args.language))
 
     # pipelines
     elif args.run_initial_pipeline:
-        run_initial_pipeline(args.language, args.num_aug, args.train_medium, args.add_copies)
-    elif args.run_k_diverse_sampling_pipeline:
-        run_k_diverse_sampling_pipeline(args.language, args.num_aug, args.train_medium)
+        run_initial_pipeline(args.language, args.train_medium, args.rand_seed)
     elif args.run_uncertainty_sampling_pipeline:
-        run_uncertainty_sampling_pipeline(args.language, args.num_aug, args.r, args.use_softmax_normalizer, args.use_high_loss, args.train_medium)
+        run_uncertainty_sampling_pipeline(args.language, args.num_aug, \
+            args.use_high_loss, args.train_medium, args.rand_seed)
     elif args.run_random_sampling_pipeline:
-        run_random_sampling_pipeline(args.language, args.num_aug, args.train_medium)
+        run_random_sampling_pipeline(args.language, args.num_aug, \
+            args.train_medium, args.rand_seed)
     elif args.run_uat_pipeline:
-        run_uat_pipeline(args.language, args.num_aug, args.train_medium, args.use_empirical, args.use_loss)
+        run_uat_pipeline(args.language, args.num_aug, args.train_medium, \
+            args.use_empirical, args.use_loss, args.rand_seed)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("language", type=str)
+    parser.add_argument("rand_seed", type=int)
     parser.add_argument("augmentation_type", type=str) # when starting out, just put "initial".
     parser.add_argument("num_aug", type=int) # when starting out, just put "initial".
     parser.add_argument("--prep_preproc_fairseq_data_initial", action='store_true')
@@ -279,22 +248,15 @@ if __name__ == "__main__":
     parser.add_argument("--run_initial_pipeline", action='store_true')
     
     # Uncertainty sampling
-    parser.add_argument("--use_softmax_normalizer", action='store_true')
-    parser.add_argument("--r", nargs='?', default=1.0, const=1.0, type=float)
-
     parser.add_argument("--use_high_loss", action='store_true')
 
     # for uat pipeline
     parser.add_argument("--use_loss", action='store_true') 
 
-    # for initial pipeline (and maybe for other pipelines, if it works well.)
-    parser.add_argument("--add_copies", action='store_true')
-
     # all options
     parser.add_argument("--train_medium", action='store_true')
 
     # Pipelines
-    parser.add_argument("--run_k_diverse_sampling_pipeline", action='store_true')
     parser.add_argument("--run_uncertainty_sampling_pipeline", action='store_true')
     parser.add_argument("--run_random_sampling_pipeline", action='store_true')
     parser.add_argument("--run_uat_pipeline", action='store_true')
