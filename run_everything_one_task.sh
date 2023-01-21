1a8d2182523c07d11af927c9bf4b5eba641a7b85
smfsamir <f.samir@mail.utoronto.ca>
2023-01-20 16:02:24 -0800

    Add script for running one task

---
+++ run_everything_one_task.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --time=50:00:00
+#SBATCH --gpus=2
+#SBATCH --mem=10G
+#SBATCH --account=rrg-mageed
+#SBATCH --output=/scratch/fsamir8/augmentation_subset_select/augmentation_everything_1node.out
+#SBATCH --error=/scratch/fsamir8/augmentation_subset_select/augmentation_everything_1node.error
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+#SBATCH --mail-user=fsamir@mail.ubc.ca
+
+# TODO: loop over everything with a range variable. feed range variable into the main_show_results.py. Resulting output: two files: all_results_form_{i} and all_results_form_{i}.csv. 
+# LANGUAGES=("bengali" "turkish" "finnish" "georgian" "arabic" "navajo" "spanish")
+
+# function run_one_iteration {
+#     SEED="$1"
+#     python main_grid_search.py $SEED --train_initial || echo "training initial models failed" && exit 1
+#     python main_grid_search.py $SEED --train_uncertainty || echo "training uncertainty models failed" exit 1
+#     python main_grid_search.py $SEED --train_random || echo "training random models failed" exit 1
+#     python main_grid_search.py $SEED --train_uat || echo "training uat models failed"
+
+#     # ## 
+#     python main_evaluate_comp_gen.py $SEED || echo "generating compositional generalization results failed" && exit 1
+#     python main_show_results.py $SEED --show_results || echo "Generating iid results failed" && exit 1
+#     python main_show_results.py $SEED --show_results_compositional || echo "Generating comp. gen results failed" && exit 1
+
+#     for lang in "${LANGUAGES[@]}"
+#     do
+#         rm -rf "/home/fsamir8/scratch/augmentation_subset_select/${lang}_${SEED}"
+#     done
+# }
+
+srun --ntasks=1 ./run_one_iteration.sh 11 &
+wait
