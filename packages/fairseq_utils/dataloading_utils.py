ae45a41ad20c200854e056499a727b63107d8c3a
smfsamir <f.samir@mail.utoronto.ca>
2023-02-19 09:17:38 -0800

    Update reading of augmentation frame

--- packages/fairseq_utils/dataloading_utils.py
+++ packages/fairseq_utils/dataloading_utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 def get_initial_generation_frame(language, aug_pool_size): # concatenation of test file and augmentation file.
     test_frame = pd.read_csv(f"{SIGM_DATA_PATH}/{language}-test", header=None, names=["src", "tgt" ,"tag"], sep='\t')
     assert len(test_frame) in [100, 1000] # NOTE: if this fails, it will be problematic. Other code (e.g., RandomSampler assumes 100 datapoints trainng points)
-    augmentation_frame = pd.read_csv(f"{HALL_DATA_PATH}/{language}-train-low-hall", header=None, names=["src", "tgt" ,"tag"], sep='\t')
+    augmentation_frame = pd.read_csv(f"{HALL_DATA_PATH}/{language}-train-low-hall-{aug_pool_size}", header=None, names=["src", "tgt" ,"tag", "invariant", "test_foreign_key"], sep='\t')
     assert len(augmentation_frame) == aug_pool_size 
 
     test_frame = pd.concat([test_frame, augmentation_frame]) # this is for the initial model, so we can get likelihoods and generations
