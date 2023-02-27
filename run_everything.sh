2761f3101d35110a225941147e07a4099ee0e12d
smfsamir <f.samir@mail.utoronto.ca>
2023-02-27 10:07:17 -0800

    modify slurm configuration

--- run_everything.sh
+++ run_everything.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=10:00:00
+#SBATCH --time=50:00:00
 #SBATCH --gpus-per-task=2
-#SBATCH --ntasks=1
+#SBATCH --ntasks=5
 #SBATCH --mem-per-cpu=10G
 #SBATCH --account=rrg-mageed
 #SBATCH --output=/scratch/fsamir8/augmentation_subset_select/augmentation_everything.out
@@ -31,8 +31,8 @@
 # }
 
 srun --ntasks=1  ./run_one_iteration.sh 0 &
-# srun --ntasks=1 ./run_one_iteration.sh 1 &
-# srun --ntasks=1 ./run_one_iteration.sh 2 &
-# srun --ntasks=1 ./run_one_iteration.sh 3 &
-# srun --ntasks=1 ./run_one_iteration.sh 4 &
+srun --ntasks=1 ./run_one_iteration.sh 1 &
+srun --ntasks=1 ./run_one_iteration.sh 2 &
+srun --ntasks=1 ./run_one_iteration.sh 3 &
+srun --ntasks=1 ./run_one_iteration.sh 4 &
 wait
