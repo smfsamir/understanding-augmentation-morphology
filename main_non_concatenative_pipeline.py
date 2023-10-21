61de597665f92e0085f4aff70a0f7a30ce694e88
smfsamir <f.samir@mail.utoronto.ca>
2023-10-20 22:59:34 -0700

    add medium setting experiment

--- main_non_concatenative_pipeline.py
+++ main_non_concatenative_pipeline.py
@@ -10,8 +10,9 @@ from flowmason import conduct, cacheable
 import subprocess
 from augment import retrieve_alignment_fails
 from dotenv import dotenv_values
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Function
 from itertools import product
+from functools import partial
 
 
 from packages.utils.util_functions import construct_cg_test_set, get_model_augment_path, load_gold_train_validation_test, tokenize_row_src, tokenize_row_tgt
@@ -102,6 +103,14 @@ def step_train_initial_model(step_name: str, version: str, augmentation_frame: p
     subprocess.run([f"{FAIRSEQ_SCRIPTS_PATH}/train_model.sh", model_augment_path, language, algorithm_type, rand_seed], check=True)
     return f"{model_augment_path}/{algorithm_type}_aug_pool_size={len(augmentation_frame)}/{language}_{algorithm_type}_model_checkpoints/checkpoint_best.pt"
 
+def step_train_initial_medium_model(step_name: str, version: str, augmentation_frame: pd.DataFrame):
+    language = 'arabic'
+    model_augment_path = get_model_augment_path(language, "initial_medium", rand_seed=0, aug_pool_size=len(augmentation_frame))
+    algorithm_type = 'initial_medium'
+    rand_seed = "0"
+    subprocess.run([f"{FAIRSEQ_SCRIPTS_PATH}/train_model.sh", model_augment_path, language, algorithm_type, rand_seed], check=True)
+    return f"{model_augment_path}/{algorithm_type}_aug_pool_size={len(augmentation_frame)}/{language}_{algorithm_type}_model_checkpoints/checkpoint_best.pt"
+
 # TODO: need to return the example number as well, so we know how to align the predictions with the source datapoint
 def evaluate_generations_from_model(generation_file_path, num_test_examples: int):
     language = 'arabic'
@@ -142,22 +151,21 @@ def evaluate_generations_from_model(generation_file_path, num_test_examples: int
 def step_generate_initial_model(step_name: str, version: str, cg_test_frame: pl.DataFrame,
                                 augmentation_frame: pd.DataFrame):
     language = 'arabic'
-    augmentation_type = 'initial'
+    augmentation_type = 'initial_medium'
     model_augment_path = get_model_augment_path(language, augmentation_type, rand_seed=0, aug_pool_size=len(augmentation_frame))
-    subprocess.run([f"{FAIRSEQ_SCRIPTS_PATH}/generate.sh", model_augment_path, language, 'initial'], check=True)
+    subprocess.run([f"{FAIRSEQ_SCRIPTS_PATH}/generate.sh", model_augment_path, language, augmentation_type], check=True)
 
     num_test_examples = len(cg_test_frame)
     num_correct, total, _ = evaluate_generations_from_model(f"{model_augment_path}/{language}_results.txt", num_test_examples)
     with open(f"{model_augment_path}/final_results.txt", 'w') as accuracy_res_f:
         accuracy_res_f.write(f"For language {language}, we obtain an accuracy of {num_correct/total} when using augmentation strategy {augmentation_type}")
     return f"{model_augment_path}/{language}_results.txt"
-    
-def step_extract_log_likelihoods_aug_pool(step_name: str, version: str, generation_results_path: str, 
-                                          cg_test_frame: pl.DataFrame, augmentation_frame: pd.DataFrame):
+
+def extract_log_likelihoods_aug_pool(cg_test_frame: pl.DataFrame, augmentation_type: str):
     aug_pool_size = 10000
     language = 'arabic'
     avg_log_likelihoods = []
-    initial_model_path = get_model_augment_path(language, 'initial', rand_seed=0, aug_pool_size=aug_pool_size)
+    initial_model_path = get_model_augment_path(language, augmentation_type, rand_seed=0, aug_pool_size=aug_pool_size)
     num_test_examples = len(cg_test_frame)
     with open(f"{initial_model_path}/{language}_results.txt", 'r') as predictions_f:
         source_line = predictions_f.readline()
@@ -177,11 +185,17 @@ def step_extract_log_likelihoods_aug_pool(step_name: str, version: str, generati
     assert found_zero_num, "The example numbers might not be zero generated"
     assert len(avg_log_likelihoods) == aug_pool_size , f"There were {len(avg_log_likelihoods)} log likelihoods collected but {aug_pool_size} were expected." 
     return avg_log_likelihoods
+    
+def step_extract_log_likelihoods_aug_pool(step_name: str, version: str,
+                                          cg_test_frame: pl.DataFrame ):
+    augmentation_type = 'initial'
+    return extract_log_likelihoods_aug_pool(cg_test_frame, augmentation_type)
 
-def step_train_augmented_model(step_name: str, version: str, 
-                               augmentation_frame: pd.DataFrame, cg_test_frame: pl.DataFrame, 
-                               avg_log_likelihoods: List[Tuple[int, float, str]]): 
-    # attach avg_log_likelihoods to the augmentation frame
+def step_extract_log_likelihoods_aug_pool_medium(step_name: str, version: str, cg_test_frame: pl.DataFrame):
+    augmentation_type = 'initial_medium'
+    return extract_log_likelihoods_aug_pool(cg_test_frame, augmentation_type)
+
+def _retrieve_augmentation_frame_with_nll(augmentation_frame: pd.DataFrame, avg_log_likelihoods: List[Tuple[int, float, str]]):
     augmentation_frame = pl.from_pandas(augmentation_frame).with_columns(
         pl.lit(list(range(len(augmentation_frame)))).alias("augmentation_index")
     )
@@ -190,20 +204,42 @@ def step_train_augmented_model(step_name: str, version: str,
         "augmentation_index": [x[0] for x in avg_log_likelihoods]
     })
     augmentation_frame = augmentation_frame.join(nll_frame, on="augmentation_index")
-    aug_pool_size = 10000
-    language = 'arabic'
-    seed = 0
+    return augmentation_frame
 
+def train_augmented_model(train_model_fn: Function[str, str, int, int], cg_test_frame: pl.DataFrame):
+    subset_sizes = [128, 512]
+    seeds = [0, 1, 2]
+    strategies = ['random']
+    cg_test_with_results_frame = cg_test_frame
+    cg_test_with_results_frame = cg_test_with_results_frame.with_columns(
+        pl.lit(list(range(len(cg_test_with_results_frame)))).alias('datapoint_index')
+    )
+    for subset_size, seed, strategy in product(subset_sizes, seeds, strategies):
+        predictions_example_nums = train_model_fn(version="005", strategy = strategy, 
+                                                  seed = seed, subset_size=subset_size)
+        prediction_frame = pl.DataFrame({
+            f"prediction_ss={subset_size}_seed={seed}_strategy={strategy}": [prediction_example_num[0] for prediction_example_num in predictions_example_nums], 
+            "datapoint_index": [prediction_example_num[1] for prediction_example_num in predictions_example_nums]
+        })
+        cg_test_with_results_frame = cg_test_with_results_frame.join(prediction_frame, on="datapoint_index")
+    return cg_test_with_results_frame
+
+def step_train_augmented_model_medium_setting(step_name: str, version: str, augmentation_frame: pd.DataFrame, 
+                                              cg_test_frame: pl.DataFrame, avg_log_likelihoods: List[Tuple[int, float, str]]):
+
+    augmentation_frame = _retrieve_augmentation_frame_with_nll(augmentation_frame, avg_log_likelihoods)
+    language = 'arabic'
+    aug_pool_size = 10000
     @cacheable(cache_dir=cache_path)
     def cacheable_train_augmented_model(cacheable_name: str, version: str,
-                                        strategy: str, seed: int, subset_size: int):
+                                        strategy: str, seed: int, subset_size: int) -> List[Tuple[str, str]]:
         if strategy == 'random':        
             augmentation_subset_frame = random_sample_augmented_data(augmentation_frame, subset_size)
         elif strategy == 'uncertainty':
             augmentation_subset_frame = highest_uncertainty_sample_augmented_data(augmentation_frame, subset_size)
         
         ## binarize
-        train_frame, validation_frame, _ = load_gold_train_validation_test(language, train_medium=False)
+        train_frame, validation_frame, _ = load_gold_train_validation_test(language, train_medium=True)
         train_frame = pl.from_pandas(train_frame) if type(train_frame) == pd.DataFrame else train_frame
         augmentation_subset_frame = pl.from_pandas(augmentation_subset_frame) if type(augmentation_subset_frame) == pd.DataFrame else augmentation_subset_frame
         model_augment_path = get_model_augment_path(language, strategy, rand_seed=seed, num_aug=subset_size, aug_pool_size=aug_pool_size)
@@ -219,25 +255,49 @@ def step_train_augmented_model(step_name: str, version: str,
         num_correct, total, predictions_example_nums = evaluate_generations_from_model(f"{model_augment_path}/{language}_results.txt", len(cg_test_frame))
         logger.info(f"Obtained an accuracy of {num_correct/total} for {strategy} with {subset_size} examples")
         return predictions_example_nums
-    
-    subset_sizes = [128, 512]
-    seeds = [0, 1, 2]
-    strategies = ['random']
-    cg_test_with_results_frame = cg_test_frame
-    cg_test_with_results_frame = cg_test_with_results_frame.with_columns(
-        pl.lit(list(range(len(cg_test_with_results_frame)))).alias('datapoint_index')
-    )
-    for subset_size, seed, strategy in product(subset_sizes, seeds, strategies):
-        predictions_example_nums = cacheable_train_augmented_model(cacheable_name=f"cacheable_train_augmented_model_{strategy}_{subset_size}_{seed}", 
-                                                      version="005", 
-                                                      strategy = strategy, 
-                                                      seed = seed, subset_size=subset_size)
-        prediction_frame = pl.DataFrame({
-            f"prediction_ss={subset_size}_seed={seed}_strategy={strategy}": [prediction_example_num[0] for prediction_example_num in predictions_example_nums], 
-            "datapoint_index": [prediction_example_num[1] for prediction_example_num in predictions_example_nums]
-        })
-        cg_test_with_results_frame = cg_test_with_results_frame.join(prediction_frame, on="datapoint_index")
-    return cg_test_with_results_frame
+    return train_augmented_model(partial(
+            cacheable_train_augmented_model, 
+            cacheable_name="cacheable_train_augmented_model_medium"
+        ), 
+    cg_test_frame)
+
+def step_train_augmented_model(step_name: str, version: str, 
+                               augmentation_frame: pd.DataFrame, cg_test_frame: pl.DataFrame, 
+                               avg_log_likelihoods: List[Tuple[int, float, str]]): 
+    # attach avg_log_likelihoods to the augmentation frame
+    aug_pool_size = 10000
+    language = 'arabic'
+
+    augmentation_frame = _retrieve_augmentation_frame_with_nll(augmentation_frame, avg_log_likelihoods)
+
+    @cacheable(cache_dir=cache_path)
+    def cacheable_train_augmented_model(cacheable_name: str, version: str,
+                                        strategy: str, seed: int, subset_size: int) -> List[Tuple[str, str]]:
+        if strategy == 'random':        
+            augmentation_subset_frame = random_sample_augmented_data(augmentation_frame, subset_size)
+        elif strategy == 'uncertainty':
+            augmentation_subset_frame = highest_uncertainty_sample_augmented_data(augmentation_frame, subset_size)
+        
+        ## binarize
+        train_frame, validation_frame, _ = load_gold_train_validation_test(language, train_medium=False)
+        train_frame = pl.from_pandas(train_frame) if isinstance(train_frame, pd.DataFrame) else train_frame
+        augmentation_subset_frame = pl.from_pandas(augmentation_subset_frame) if type(augmentation_subset_frame) == pd.DataFrame else augmentation_subset_frame
+        model_augment_path = get_model_augment_path(language, strategy, rand_seed=seed, num_aug=subset_size, aug_pool_size=aug_pool_size)
+        train_frame = pl.concat([train_frame, augmentation_subset_frame.select(['src', 'tgt', 'tag'])])
+        _binarize_data(train_frame, validation_frame, cg_test_frame, model_augment_path)
+
+        ## train    
+        algorithm_type = strategy
+        subprocess.run([f"{FAIRSEQ_SCRIPTS_PATH}/train_model.sh", model_augment_path, language, algorithm_type, str(seed)], check=True)
+
+        ## generate
+        subprocess.run([f"{FAIRSEQ_SCRIPTS_PATH}/generate.sh", model_augment_path, language, algorithm_type], check=True)
+        num_correct, total, predictions_example_nums = evaluate_generations_from_model(f"{model_augment_path}/{language}_results.txt", len(cg_test_frame))
+        logger.info(f"Obtained an accuracy of {num_correct/total} for {strategy} with {subset_size} examples")
+        return predictions_example_nums
+    return train_augmented_model(partial(cacheable_train_augmented_model,
+        cacheable_name="cacheable_train_augmented_model"), 
+    cg_test_frame)
 
 # TODO: Fill in.
 def step_compute_accuracy_on_unaligned_datapoints(step_name: str, 
@@ -268,18 +328,32 @@ if __name__ == "__main__":
         "step_name": "step_load_arabic_test_dataset",
         "version": "001",
     })
+    steps['load_medium_excl_arabic_test_dataset'] = (step_load_medium_arabic_test_dataset, {
+        "step_name": "step_load_medium_arabic_test_dataset",
+        "version": "001",
+    })
     steps['load_non_concat_examples'] = (step_load_non_concat_examples, {
         "step_name": "step_load_non_concat_examples",
         "version": "003",
         "cg_test_set_arabic_frame": "load_arabic_test_dataset",
     })
-    steps["generate_augmented_data"] = (step_generate_augmented_data, {
+    steps['load_non_concat_examples_medium'] = (step_load_non_concat_examples, {
+        "step_name": "step_load_non_concat_examples_medium",
+        "version": "001",
+        "cg_test_set_arabic_frame": "load_medium_excl_arabic_test_dataset",
+    })
+    steps["generate_augmented_data"] = (step_generate_augmented_data, { # this is fixed for whatevre choice of initial training data
         "step_name": "step_generate_augmented_data",
         "version": "001",
     })
-    steps["binarize_initial_data"] = (step_binarize_initial_training_and_eval_data, {
+    steps["binarize_initial_data"] = (step_binarize_initial_training_and_eval_data, { 
         "version": "001",
         "cg_test_frame": "load_non_concat_examples",
+        "augmentation_frame": "generate_augmented_data"
+    })
+    steps["binarize_initial_medium_data"] = (step_binarize_medium_training_and_eval_data, {
+        "version": "001",
+        "cg_test_frame": "load_non_concat_examples_medium",
         "augmentation_frame": "generate_augmented_data"
     })
     steps['train_initial_model'] = (step_train_initial_model, {
@@ -287,11 +361,23 @@ if __name__ == "__main__":
         "version": "001",
         "augmentation_frame": "generate_augmented_data"
     })
+    steps['train_initial_medium_model'] = (step_train_initial_medium_model, {
+        "step_name": "step_train_initial_medium_model",
+        "version": "001",
+        "augmentation_frame": "generate_augmented_data"
+    })
+
     # add step for generating from initial model
     steps['generate_initial_model'] = (step_generate_initial_model, {
         "step_name": "step_generate_initial_model",
         "version": "001",
         "cg_test_frame": "load_non_concat_examples",
+        "augmentation_frame": "generate_augmented_data"
+    })
+    steps['generate_initial_model_medium'] = (step_generate_initial_model, {
+        "step_name": "step_generate_initial_model_medium",
+        "version": "001",
+        "cg_test_frame": "load_non_concat_examples_medium",
         "augmentation_frame": "generate_augmented_data"
     })
     # add step for extracting log likelihoods from initial model
@@ -302,6 +388,11 @@ if __name__ == "__main__":
         "cg_test_frame": "load_non_concat_examples",
         "augmentation_frame": "generate_augmented_data"
     })
+    steps['extract_log_likelihoods_aug_pool_medium'] = (step_extract_log_likelihoods_aug_pool_medium, {
+        "step_name": "step_extract_log_likelihoods_aug_pool_medium",
+        "version": "001",
+        "cg_test_frame": "load_non_concat_examples_medium",
+    })
     # add step for training with augmented data
     steps['train_augmented_model'] = (step_train_augmented_model, {
         "step_name": "step_train_augmented_model",
@@ -309,6 +400,13 @@ if __name__ == "__main__":
         "cg_test_frame": "load_non_concat_examples",
         "augmentation_frame": "generate_augmented_data",
         "avg_log_likelihoods": "extract_log_likelihoods_aug_pool"
+    })
+    steps['train_augmented_model_medium'] = (step_train_augmented_model_medium_setting, {
+        "step_name": "step_train_augmented_model_medium",
+        "version": "001",
+        "cg_test_frame": "load_non_concat_examples_medium",
+        "augmentation_frame": "generate_augmented_data",
+        "avg_log_likelihoods": "extract_log_likelihoods_aug_pool_medium"
     })
     steps['compute_accuracy_on_unaligned_datapoints'] = (step_compute_accuracy_on_unaligned_datapoints, {
         "step_name": "step_compute_accuracy_unaligned_data", 
